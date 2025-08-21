365da972eb6aa47bb279379d95856fa8dbd7ef5a
Artur Flis <office@bluee.dev>
2025-08-21 16:40:09 +0200

    Update main.go

--- app/main.go
+++ app/main.go
@@ -388,7 +388,7 @@ func main() {
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
+		port = "9911"
 	}
 	log.Printf("Server starting on port %s", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
